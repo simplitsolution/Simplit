d947ca361f845e9c85b503893d80011f4ed2388f
Vishal <cubes.dev7@gmail.com>
2025-07-29 15:27:37 +0530

    added changes for experties page

--- src/app/pages/expertise/expertise.ts
+++ src/app/pages/expertise/expertise.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, AfterViewInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { serviceCardsDetail } from '../../shared/utils/data';
 
 @Component({
   selector: 'app-expertise',
@@ -12,57 +13,8 @@ import { RouterModule } from '@angular/router';
 export class Expertise implements AfterViewInit {
   isVisible = signal(false);
 
- serviceCards = [
-  {
-    title: 'Mobile App Development',
-    icon: '../../../assets/images/mobile-development.svg',
-    sections: [
-      {
-        heading: 'Native App Development',
-        items: ['iOS Application Development', 'Android Application Development', 'Tizen Application Development']
-      },
-      {
-        heading: 'Hybrid App Development',
-        items: ['Flutter Application Development', 'React Native Application Development']
-      }
-    ],
-    link: '#',
-  },
-  {
-    title: 'Web Development',
-    icon: '../../../assets/images/web-development.svg',
-    sections: [
-      {
-        heading: 'Backend Development',
-        items: ['Node JS, Mongo-DB', 'Java Web Development', 'PHP/Laravel, MySQL']
-      },
-      {
-        heading: 'Front End Development',
-        items: ['HTML/CSS/JavaScript', 'Angular, React, Vue']
-      }
-    ],
-    link: '#',
-  },
-  {
-    title: 'SEO & Digital Marketing',
-    icon: '../../../assets/images/MVP-development-service-icon.svg',
-    sections: [
-      {
-        heading: 'SEO',
-        items: ['On Page SEO', 'Off Page SEO', 'Website Optimization']
-      },
-      {
-        heading: 'Digital Marketing',
-        items: ['Social Media Marketing', 'Blog Submission']
-      }
-    ],
-    link: '#'
-  }
-];
-
-
-
-  constructor(private el: ElementRef) {}
+ serviceCards = serviceCardsDetail;
+ constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
     if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
--- src/app/shared/utils/data.ts
+++ src/app/shared/utils/data.ts
@@ -20,3 +20,51 @@ export const navbarDetails = [
         link: '/careers'
     }
 ]
+
+export const serviceCardsDetail = [
+  {
+    title: 'Mobile App Development',
+    icon: '../../../assets/images/Adobe-Express.png',
+    sections: [
+      {
+        heading: 'Native App Development',
+        items: ['iOS Application Development', 'Android Application Development', 'Tizen Application Development']
+      },
+      {
+        heading: 'Hybrid App Development',
+        items: ['Flutter Application Development', 'React Native Application Development']
+      }
+    ],
+    link: '#',
+  },
+  {
+    title: 'Web Development',
+    icon: '../../../assets/images/web-development.svg',
+    sections: [
+      {
+        heading: 'Backend Development',
+        items: ['Node JS, Mongo-DB', 'Java Web Development', 'PHP/Laravel, MySQL']
+      },
+      {
+        heading: 'Front End Development',
+        items: ['HTML/CSS/JavaScript', 'Angular, React, Vue']
+      }
+    ],
+    link: '#',
+  },
+  {
+    title: 'SEO & Digital Marketing',
+    icon: '../../../assets/images/MVP-development-service-icon.svg',
+    sections: [
+      {
+        heading: 'SEO',
+        items: ['On Page SEO', 'Off Page SEO', 'Website Optimization']
+      },
+      {
+        heading: 'Digital Marketing',
+        items: ['Social Media Marketing', 'Blog Submission']
+      }
+    ],
+    link: '#'
+  }
+];
