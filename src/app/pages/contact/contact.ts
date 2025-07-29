b98c22f19161b3b9e7e3f7e0c8dccd27fd156dd7
simplitsolution <simplitsolution.in@gmail.com>
2025-07-29 17:56:33 +0530

    added contact details

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss'
 })
 export class Contact {
+  contactInfo = {
+    address: {
+      line1: ' 332 - Skyview Business Horizon',
+      line2: 'Surat - Kamrej Hwy, opp. Dreamland Party Plot',
+      line3: 'Surat, Gujarat 395006'
+    },
+    phone: '+91 84605 50273',
+    email: 'hr@Simplit.com'
+  };
 
 }
