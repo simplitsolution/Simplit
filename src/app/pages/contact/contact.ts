ee5283c8ebe47fcc936b77baec163f3a221294cf
simplitsolution <simplitsolution.in@gmail.com>
2025-07-30 18:56:06 +0530

    added animations and code optimization

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal } from '@angular/core';
+import { contactCardDetail } from '../../shared/utils/data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,RouterModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
-export class Contact {
-  contactInfo = {
-    address: {
-      line1: ' 332 - Skyview Business Horizon',
-      line2: 'Surat - Kamrej Hwy, opp. Dreamland Party Plot',
-      line3: 'Surat, Gujarat 395006'
-    },
-    phone: '+91 84605 50273',
-    email: 'hr@Simplit.com'
-  };
+export class Contact implements AfterViewInit {
+  isVisible = signal(false);
 
+  contactInfo = contactCardDetail;
+  constructor(private el: ElementRef) {}
+
+    ngAfterViewInit() {
+      if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+        const observer = new IntersectionObserver(
+          entries => {
+            entries.forEach(entry => {
+              this.isVisible.set(entry.isIntersecting);
+            });
+          },
+          { threshold: 0.3 }
+        );
+
+        observer.observe(this.el.nativeElement);
+      }
+    }
 }
