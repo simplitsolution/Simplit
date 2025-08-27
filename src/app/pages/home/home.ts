9c2c91a6c586d6000c422fab13cbd45c1ddd078f
Maulika Vasani <vasanimaulika182@gmail.com>
2025-08-27 16:18:09 +0530

    change the video and remove SSR

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
@@ -8,6 +8,29 @@ import { BrowserModule } from '@angular/platform-browser';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements AfterViewInit {
+  @ViewChild('myVideo') videoRef!: ElementRef<HTMLVideoElement>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const videoEl = this.videoRef?.nativeElement;
+
+      if (videoEl instanceof HTMLVideoElement) {
+        videoEl.autoplay = true;
+        videoEl.loop = true;
+        videoEl.muted = true;
+
+        videoEl.load();
+
+        videoEl.addEventListener('loadeddata', () => {
+          videoEl.play().catch(err => {
+            console.warn('Autoplay failed after reload:', err);
+          });
+        });
+      }
+    }
+  }
+
 
 }
